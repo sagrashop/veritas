88c6e37ff241d202f1893d7e0fc09254a5047f73
giuseppe <sagrashopcatania@gmail.com>
2026-09-15 18:35:36 +0200

    Aggiornamento automatico n. 34

--- lib/main.dart
+++ lib/main.dart
@@ -720,14 +720,13 @@ class _FeedScreenState extends State<FeedScreen> {
                                   ),
                                 ],
                               ),
-                             (post['media'] != null && post['media'].toString().isNotEmpty)
-    ? Padding(
-        padding: const EdgeInsets.only(top: 8.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: _buildImageWidget(post['media']),
-        ),
-      )
+                             (post['media'] != null && post['media'].toString().isNotEmpty) ? Padding(
+  padding: const EdgeInsets.only(top: 8.0),
+  child: ClipRRect(
+    borderRadius: BorderRadius.circular(12),
+    child: _buildImageWidget(post['media']),
+  ),
+)
     : Text(
         post['content'] ?? '',
         style: const TextStyle(
